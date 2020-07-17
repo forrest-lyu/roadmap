0444b34715b30497f669218094eb1bd2187953b4
Forrest Lyu <forrest.lyu.dev@gmail.com>
2020-07-17 15:21:41 +0800

    create ImageViewer.java

--- 2-back-end/learning-notes/0-core-java/corejava-forrest/v1ch02/imageviewer/ImageViewer.java
+++ 2-back-end/learning-notes/0-core-java/corejava-forrest/v1ch02/imageviewer/ImageViewer.java
@@ -1,12 +1,12 @@
 package v1ch02.imageviewer;
 
-import java.awt.*;
 import java.io.*;
+import java.awt.*;
 import javax.swing.*;
 
 /**
- * A program for viewing images.
- * @version 1.0 2020-07-14
+ * A program is for showing images.
+ * @version 1.0
  * @author Forrest Lyu <forrest.lyu.dev@gmail.com>
  */
 public class ImageViewer
@@ -14,36 +14,37 @@ public class ImageViewer
     public static void main(String[] args)
     {
         EventQueue.invokeLater(() -> {
-            var myImageViewer = new MyImageViewer();
-            myImageViewer.setTitle("ImageViewer");
-            myImageViewer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            myImageViewer.setVisible(true);
+            var imageViewerFrame = new ImageViewerFrame();
+            imageViewerFrame.setTitle("ImageViewer");
+            imageViewerFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            imageViewerFrame.setVisible(true);
         });
     }
 }
 
 /**
- * A frame with a label to show an image.
+ * A class that is inherited from JFrame.
+ * @version 1.0
+ * @author Forrest Lyu <forrest.lyu.dev@gmail.com>
  */
-class MyImageViewer extends JFrame
+class ImageViewerFrame extends JFrame
 {
     private static final int DEFAULT_WIDTH = 300;
     private static final int DEFAULT_HEIGHT = 400;
 
-    public MyImageViewer()
+    public ImageViewerFrame()
     {
         setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-        setLocationRelativeTo(null);
 
-        // 1. Use a label to display the images
+        // 1. Use a label to display the images.
         var label = new JLabel();
         add(label);
 
-        // 2. Set up the file chooser
-        var chooser = new JFileChooser();
-        chooser.setCurrentDirectory(new File("."));
+        // 2. Set up the file chooser.
+        var fileChooser = new JFileChooser();
+        fileChooser.setCurrentDirectory(new File("."));
 
-        // 3. Set up the menu bar
+        // 3. Set up the menu bar.
         var menuBar = new JMenuBar();
         setJMenuBar(menuBar);
 
@@ -53,14 +54,14 @@ class MyImageViewer extends JFrame
         var openItem = new JMenuItem("Open");
         menu.add(openItem);
         openItem.addActionListener(event -> {
-            // 1. Show file chooser dialog
-            int result = chooser.showOpenDialog(null);
+            // 1. Show file chooser dialog.
+            int result = fileChooser.showOpenDialog(null);
 
-            // 2. If file selected, set it as icon of the label
+            // 2. If file selected, set it as icon of the label.
             if (result == JFileChooser.APPROVE_OPTION)
             {
-                String name = chooser.getSelectedFile().getPath();
-                label.setIcon(new ImageIcon(name));
+                String filePath = fileChooser.getSelectedFile().getPath();
+                label.setIcon(new ImageIcon(filePath));
             }
         });
 
