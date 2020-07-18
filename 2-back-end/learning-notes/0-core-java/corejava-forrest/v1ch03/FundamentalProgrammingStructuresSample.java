4ed70c53c316c77f8c3ea906443b62ec4af254db
Forrest Lyu <forrest.lyu.dev@gmail.com>
2020-07-18 15:41:39 +0800

    modify pdf file

---
+++ 2-back-end/learning-notes/0-core-java/corejava-forrest/v1ch03/FundamentalProgrammingStructuresSample.java
@@ -0,0 +1,62 @@
+package v1ch03;
+
+/**
+ * This program shows the fundamental programming structures in Java.
+ * @version 1.0
+ * @author Forrest Lyu <forrest.lyu.dev@gmail.com>
+ */
+public class FundamentalProgrammingStructuresSample
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This program shows the fundamental programming " + 
+                "structures in Java");
+
+        /*
+         * 1. Comments:
+         *  - Single comment
+         *  - Multiple lines' comments
+         *  - Documentation comment
+         *
+         * 2. Data types
+         *  - Primitive data types:
+         *      - byte (8 bits):    -128 ~ 127
+         *      - short (16 bits):  -32768 ~ 32767
+         *      - int (32 bits):    -2.1 billion ~ 2.1 billion (-2.1E9 ~ 2.1E9)
+         *      - long (64 bits):   -9.2 billion billion ~ 9.2 billion billion
+         *                          (-9.2E18 ~ 9.2E18)
+         *      
+         *      - float (4 bytes):
+         *      - double (8 bytes):
+         *
+         *      - char (1 bytes):
+         *
+         *      - boolean:          true, false
+         *                          (null, true, false is literals, not keywords) 
+         *
+         *  - Reference data types:
+         *      - Objects: String, StringBuilder
+         *
+         * 3. Operators
+         *  - Assignment statement
+         *  - Arithmetic statement
+         *  - Comparison statement
+         *  - Logical statement
+         *  - Bitwise statement
+         *
+         * 4. Strings
+         * 5. Input and Output
+         * 6. Big numbers
+         * 7. Arrays
+         * 
+         * appendix A) literals
+         *  - numeric
+         *  - character
+         *  - boolean (true, false)
+         *  - string
+         *  - null
+         */
+
+
+    }
+}
