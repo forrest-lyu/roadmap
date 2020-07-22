995b3a9ee365d1cfefdb1518fa8000c6806325cf
Forrest Lyu <forrest.lyu.dev@gmail.com>
2020-07-22 14:26:29 +0800

    vertify how to edit binary file by using vim.

---
+++ 2-back-end/learning-notes/0-core-java/corejava-forrest/checksum-vertify/FirstSample.java
@@ -0,0 +1,8 @@
+package v1ch03.FirstSample;
+
+public class FirstSample
+{
+    public static void main(String[] args)
+    {
+    }
+}
---
+++ 2-back-end/learning-notes/0-core-java/corejava-forrest/v1ch03/FirstSample/FirstSample.java
@@ -0,0 +1,9 @@
+package v1ch03.FirstSample;
+
+public class FirstSample
+{
+    public static void main(String[] args)
+    {
+        System.out.println("We don't use 'Hello World'!");
+    }
+}
